1fccb6306fa7c3588ec63fb665267b30a00ea10d
João Pedro Bacarin <112831294+JPBaccarin@users.noreply.github.com>
2023-12-27 16:41:39 -0300

    adição do prettier para limpeza de código

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,22 +1,28 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar/navbar";
+import Footer from "@/components/footer/footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Dani Festas',
-  description: 'Decoração de Festas Infantis - Adulto - Eventos Corporativos',
-}
+  title: "Dani Festas",
+  description: "Decoração de Festas Infantis - Adulto - Eventos Corporativos",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
+      <Footer />
     </html>
-  )
+  );
 }
