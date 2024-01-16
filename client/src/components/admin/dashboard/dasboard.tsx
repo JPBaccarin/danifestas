6b08f8a7e9077a8575b7291d0e320b5024f8c707
Felipinho <santosjesussilvafelipe@gmail.com>
2024-01-15 21:27:16 -0300

    Filtro dinâmico

--- client/src/components/admin/dashboard/dasboard.tsx
+++ client/src/components/admin/dashboard/dasboard.tsx
@@ -1,11 +1,12 @@
 // components/Dashboard.tsx
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import CardDashboard from "./card/carddashboard";
 import EditDecorationDialog from "./buttons/editbutton";
 import DeleteDecorationDialog from "./buttons/deletebutton";
 import { AlertDialog } from "@/components/ui/alert-dialog";
+import Filter3, { FilteredData } from "@/components/decoracoes/filter/filter3";
 
 interface Decoration {
   id_deco: number;
@@ -18,13 +19,37 @@ interface Decoration {
 
 const Dashboard: React.FC = () => {
   const [decorations, setDecorations] = useState<Decoration[]>([]);
+  const [filteredDecorations, setFilteredDecorations] = useState<Decoration[]>(
+    [],
+  );
+  const [uniqueCategories, setUniqueCategories] = useState<string[]>([]);
+  const [uniqueTypes, setUniqueTypes] = useState<string[]>([]);
+  const [uniqueThemes, setUniqueThemes] = useState<string[]>([]);
 
   const fetchData = async () => {
     try {
       const response = await axios.get<Decoration[]>(
-        "http://localhost:3003/decorations/"
+        "http://localhost:3003/decorations/",
       );
       setDecorations(response.data);
+
+      // Extrai opções únicas de categorias, tipos e temas
+      const categories = Array.from(
+        new Set(response.data.map((item) => item.categoria || "")),
+      );
+      const types = Array.from(
+        new Set(response.data.map((item) => item.tipo || "")),
+      );
+      const themes = Array.from(
+        new Set(response.data.map((item) => item.tema || "")),
+      );
+
+      setUniqueCategories(["", ...categories]);
+      setUniqueTypes(["", ...types]);
+      setUniqueThemes(["", ...themes]);
+
+      // Inicializa as decorações filtradas com todas as decorações
+      setFilteredDecorations(response.data);
     } catch (error) {
       console.error("Erro ao obter decorações:", error);
     }
@@ -34,34 +59,64 @@ const Dashboard: React.FC = () => {
     fetchData();
   }, []);
 
-  return (
-    <div className="grid grid-cols-1 gap-4 sm:m-5 sm:grid-cols-2 md:grid-cols-3">
-      {decorations.map((decoration) => (
-        <CardDashboard
-          key={decoration.id_deco}
-          category={decoration.categoria}
-          title={decoration.titulo}
-          theme={decoration.tema}
-          type={decoration.tipo}
-          images={decoration.images.map(
-            (image) => `http://localhost:3003/${image}`
-          )}
-        >
-          <div className="mt-2 flex justify-around ">
-            <EditDecorationDialog
-              decorationId={decoration.id_deco}
-              onEditSuccess={fetchData}
-            />
+  const handleFilterChange = useCallback(
+    (filterData: FilteredData) => {
+      const filteredItems = decorations.filter((item) => {
+        const categoryMatch =
+          filterData.category === "" || item.categoria === filterData.category;
+        const typeMatch =
+          filterData.type === "" || item.tipo === filterData.type;
+        const themeMatch =
+          filterData.theme === "" || item.tema === filterData.theme;
+        const searchMatch =
+          item.titulo.toLowerCase().includes(filterData.search.toLowerCase()) ||
+          (item.tipo &&
+            item.tipo.toLowerCase().includes(filterData.search.toLowerCase()));
 
-            <AlertDialog>
-              <DeleteDecorationDialog
+        return categoryMatch && typeMatch && themeMatch && searchMatch;
+      });
+
+      setFilteredDecorations(filteredItems);
+    },
+    [decorations],
+  );
+
+  return (
+    <div>
+      <Filter3
+        categories={uniqueCategories}
+        types={uniqueTypes}
+        themes={uniqueThemes}
+        onFilterChange={handleFilterChange}
+      />
+      <div className="grid grid-cols-1 gap-4 sm:m-5 sm:grid-cols-2 md:grid-cols-3">
+        {filteredDecorations.map((decoration) => (
+          <CardDashboard
+            key={decoration.id_deco}
+            category={decoration.categoria}
+            title={decoration.titulo}
+            theme={decoration.tema}
+            type={decoration.tipo}
+            images={decoration.images.map(
+              (image) => `http://localhost:3003/${image}`,
+            )}
+          >
+            <div className="mt-2 flex justify-around ">
+              <EditDecorationDialog
                 decorationId={decoration.id_deco}
-                onDeleteSuccess={fetchData}
+                onEditSuccess={fetchData}
               />
-            </AlertDialog>
-          </div>
-        </CardDashboard>
-      ))}
+
+              <AlertDialog>
+                <DeleteDecorationDialog
+                  decorationId={decoration.id_deco}
+                  onDeleteSuccess={fetchData}
+                />
+              </AlertDialog>
+            </div>
+          </CardDashboard>
+        ))}
+      </div>
     </div>
   );
 };
--- client/src/components/decoracoes/filter/filter3.tsx
+++ client/src/components/decoracoes/filter/filter3.tsx
@@ -1,37 +1,75 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Input } from "@/components/ui/input";
 
 export interface Filter3Props {
   categories: string[];
   types: string[];
+  themes: string[];
   onFilterChange: (filterData: FilteredData) => void;
 }
 
 export interface FilteredData {
   category: string;
   type: string;
+  theme: string;
   search: string;
 }
 
 const Filter3: React.FC<Filter3Props> = ({
   categories,
   types,
+  themes,
   onFilterChange,
 }) => {
   const [filterData, setFilterData] = useState<FilteredData>({
     category: "",
     type: "",
+    theme: "",
     search: "",
   });
 
   const [showDropdown, setShowDropdown] = useState(false);
+  const [expandCategory, setExpandCategory] = useState(false);
+  const [expandType, setExpandType] = useState(false);
+  const [expandTheme, setExpandTheme] = useState(false);
+
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     onFilterChange(filterData);
   }, [filterData, onFilterChange]);
 
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node) 
+      ) {
+        setShowDropdown(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
   const toggleDropdown = () => {
     setShowDropdown((prevShowDropdown) => !prevShowDropdown);
+  };
+
+  const toggleExpandCategory = () => {
+    setExpandCategory((prevExpand) => !prevExpand);
+  };
+
+  const toggleExpandType = () => {
+    setExpandType((prevExpand) => !prevExpand);
+  };
+
+  const toggleExpandTheme = () => {
+    setExpandTheme((prevExpand) => !prevExpand);
   };
 
   const handleRadioChange = (key: string, value: string) => {
@@ -41,10 +79,13 @@ const Filter3: React.FC<Filter3Props> = ({
     }));
     setShowDropdown(false);
   };
-  
+
   return (
     <div className="m-5 mb-5 flex flex-col items-center justify-between gap-2">
-      <div className="m2 relative mb-5 flex w-full  items-center justify-between gap-2 sm:flex-row">
+      <div
+        className="m2 relative mb-5 flex w-full items-center justify-between gap-2 sm:flex-row"
+        ref={dropdownRef}
+      >
         <Input
           type="text"
           placeholder="Pesquisar..."
@@ -84,37 +125,79 @@ const Filter3: React.FC<Filter3Props> = ({
           </svg>
         </button>
         {showDropdown && (
-          <div className="absolute right-0 z-10 mt-1 flex h-96 sm:w-96 translate-x-2 translate-y-[215px] flex-col gap-2 divide-gray-100 overflow-y-auto rounded-lg bg-white p-4 accent-[#E39CC1] shadow dark:bg-gray-700">
-            <h1>Tipo</h1>
-            {categories.map((category, index) => (
-              <div key={index} className="mt-1 flex items-center gap-2">
-                <input
-                  type="radio"
-                  value={category}
-                  checked={filterData.category === category}
-                  onChange={() => handleRadioChange("category", category)}
-                  className="h-4 w-4 border border-pink-300 bg-gray-100 focus:ring-2 focus:ring-[#dd8eb7]"
-                />
-                <label className="text-sm font-medium text-gray-900 dark:text-gray-300">
-                  {category || "Todas as Categorias"}
-                </label>
-              </div>
-            ))}
-            <h1 className="mt-6">Modelo</h1>
-            {types.map((type, index) => (
-              <div key={index} className="mt-1 flex items-center gap-2">
-                <input
-                  type="radio"
-                  value={type}
-                  checked={filterData.type === type}
-                  onChange={() => handleRadioChange("type", type)}
-                  className="h-4 w-4 border border-pink-300 bg-gray-100 focus:ring-2 focus:ring-[#dd8eb7]"
-                />
-                <label className="text-sm font-medium text-gray-900 dark:text-gray-300">
-                  {type || "Todos os Tipos"}
-                </label>
-              </div>
-            ))}
+          <div className="absolute right-0 z-10 mt-1 flex h-96 max-h-96 translate-x-2 translate-y-[215px] flex-col gap-2 divide-gray-100 overflow-y-auto rounded-lg bg-white p-4 accent-[#E39CC1] shadow sm:w-96 dark:bg-gray-700">
+            <div className="flex flex-col gap-2">
+              <h1
+                className="flex cursor-pointer items-center justify-between border-b-2 border-foreground/60 p-[6px]"
+                onClick={toggleExpandCategory}
+              >
+                Categoria
+                <span>{expandCategory ? "-" : "+"}</span>
+              </h1>
+              {expandCategory &&
+                categories.map((category, index) => (
+                  <div key={index} className="mt-1 flex items-center gap-2 pl-[6px]">
+                    <input
+                      type="radio"
+                      value={category}
+                      checked={filterData.category === category}
+                      onChange={() => handleRadioChange("category", category)}
+                      className="h-4 w-4 border border-pink-300 bg-gray-100 focus:ring-2 focus:ring-[#dd8eb7]"
+                    />
+                    <label className="text-sm font-medium text-gray-900 dark:text-gray-300">
+                      {category || "Todas as Categorias"}
+                    </label>
+                  </div>
+                ))}
+            </div>
+            <div className="flex flex-col gap-2">
+              <h1
+                className="flex cursor-pointer items-center justify-between border-b-2 border-foreground/60 p-[6px]"
+                onClick={toggleExpandType}
+              >
+                Modelo
+                <span>{expandType ? "-" : "+"}</span>
+              </h1>
+              {expandType &&
+                types.map((type, index) => (
+                  <div key={index} className="mt-1 flex items-center gap-2 pl-[6px]">
+                    <input
+                      type="radio"
+                      value={type}
+                      checked={filterData.type === type}
+                      onChange={() => handleRadioChange("type", type)}
+                      className="h-4 w-4 border border-pink-300 bg-gray-100 focus:ring-2 focus:ring-[#dd8eb7]"
+                    />
+                    <label className="text-sm font-medium text-gray-900 dark:text-gray-300">
+                      {type || "Todos os Tipos"}
+                    </label>
+                  </div>
+                ))}
+            </div>
+            <div className="flex flex-col gap-2">
+              <h1
+                className="flex cursor-pointer items-center justify-between border-b-2 border-foreground/60 p-[6px]"
+                onClick={toggleExpandTheme}
+              >
+                Tema
+                <span>{expandTheme ? "-" : "+"}</span>
+              </h1>
+              {expandTheme &&
+                themes.map((theme, index) => (
+                  <div key={index} className="mt-1 flex items-center gap-2 pl-[6px]">
+                    <input
+                      type="radio"
+                      value={theme}
+                      checked={filterData.theme === theme}
+                      onChange={() => handleRadioChange("theme", theme)}
+                      className="h-4 w-4 border border-pink-300 bg-gray-100 focus:ring-2 focus:ring-[#dd8eb7]"
+                    />
+                    <label className="text-sm font-medium text-gray-900 dark:text-gray-300">
+                      {theme || "Todos os Temas"}
+                    </label>
+                  </div>
+                ))}
+            </div>
           </div>
         )}
       </div>
